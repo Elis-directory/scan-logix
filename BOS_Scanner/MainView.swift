5b91b0285991fdfa0ff3314728b31c9bf0cdfa3e
Elis-directory <echomoshe001@gmail.com>
2024-08-29 21:48:21 -0700

    fixed nav bar issues
    switched from custom state manager to tabview

--- BOS_Scanner/MainView.swift
+++ BOS_Scanner/MainView.swift
@@ -21,61 +21,125 @@ struct MainView: View {
     @State private var scannedUPC: String? = nil
     @State var newListItem: String = ""
     @State private var addItemSheet = false
-
+    
     var body: some View {
         VStack {
             Title()
-            NavigationStack {
-                switch currentView {
-                case .homeState:
+            TabView {
+                NavigationStack {
                     HomePage()
-                case .scanState:
-                    BarcodeScannerView(
-                        items: items,
-                        isPresenting: $isPresentingScanner,
-                        matchedItem: $scannedItem,
-                        showAddItemView: $showAddItemView,
-                        scannedUPC: $scannedUPC // Pass the scanned UPC to BarcodeScannerView
-                    )
-                    .fullScreenCover(item: $scannedItem) { item in
-                        NavigationStack {
-                            DisplayEntry(item: .constant(item))
-                        }
-                    }
-                    .alert(isPresented: $showAddItemView) {
-                        
-                        Alert(
-                            title: Text("No Match Found"),
-                            message: Text("The scanned barcode does not match any item in the database. Would you like to add a new item?"),
-                            primaryButton: .default(Text("Add New Item")) {
-                                showAddItemView = false // Dismiss the alert
-                                isPresentingScanner = false // Dismiss the scanner
-                                addItemSheet = true // Trigger the presentation of the AddItemView
-                            },
-                            secondaryButton: .cancel {
-                                showAddItemView = false
-                                isPresentingScanner = false // Dismiss the scanner
-                                currentView = .homeState
-                            }
-                        )
-                    }
-                    .sheet(isPresented: $addItemSheet) {
-                        AddItemView(item: $newListItem, upc: scannedUPC ?? "") // Provide a default value if scannedUPC is nil
-                        
-                    }
-                  
-
-                case .searchState:
-                    SearchPage()
-                case .settingsState:
-                    SettingsPage()
                 }
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+                
+                
+                BarcodeScannerView(
+                    items: items,
+                    isPresenting: $isPresentingScanner,
+                    matchedItem: $scannedItem,
+                    showAddItemView: $showAddItemView,
+                    scannedUPC: $scannedUPC // Pass the scanned UPC to BarcodeScannerView
+                )
+                .fullScreenCover(item: $scannedItem) { item in
+                    NavigationStack {
+                        DisplayEntry(item: .constant(item))
+                    }
+                }
+                .alert(isPresented: $showAddItemView) {
+                    
+                    Alert(
+                        title: Text("No Match Found"),
+                        message: Text("The scanned barcode does not match any item in the database. Would you like to add a new item?"),
+                        primaryButton: .default(Text("Add New Item")) {
+                            showAddItemView = false
+                            isPresentingScanner = false
+                            addItemSheet = true
+                        },
+                        secondaryButton: .cancel {
+                            showAddItemView = false
+                            isPresentingScanner = false
+                            
+                        }
+                    )
+                }
+                .sheet(isPresented: $addItemSheet) {
+                    AddItemView(item: $newListItem, upc: scannedUPC ?? "")
+                }
+                .tabItem {
+                    Label("Scan", systemImage: "barcode.viewfinder")
+                }
+                
+                SearchPage()
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                
+                SettingsPage()
+                    .tabItem {
+                        Label("Settings", systemImage: "gearshape")
+                    }
             }
-            NavBarView(x: 32, y: 32, displayState: $currentView)
+            
+            
+            
         }
     }
 }
 
+           
+            
+                
+//                switch currentView {
+//                case .homeState:
+//                    HomePage()
+//                case .scanState:
+//                    BarcodeScannerView(
+//                        items: items,
+//                        isPresenting: $isPresentingScanner,
+//                        matchedItem: $scannedItem,
+//                        showAddItemView: $showAddItemView,
+//                        scannedUPC: $scannedUPC // Pass the scanned UPC to BarcodeScannerView
+//                    )
+//                    .fullScreenCover(item: $scannedItem) { item in
+//                        NavigationStack {
+//                            DisplayEntry(item: .constant(item))
+//                        }
+//                    }
+//                    .alert(isPresented: $showAddItemView) {
+//                        
+//                        Alert(
+//                            title: Text("No Match Found"),
+//                            message: Text("The scanned barcode does not match any item in the database. Would you like to add a new item?"),
+//                            primaryButton: .default(Text("Add New Item")) {
+//                                showAddItemView = false // Dismiss the alert
+//                                isPresentingScanner = false // Dismiss the scanner
+//                                addItemSheet = true // Trigger the presentation of the AddItemView
+//                            },
+//                            secondaryButton: .cancel {
+//                                showAddItemView = false
+//                                isPresentingScanner = false // Dismiss the scanner
+//                                currentView = .homeState
+//                            }
+//                        )
+//                    }
+//                    .sheet(isPresented: $addItemSheet) {
+//                        AddItemView(item: $newListItem, upc: scannedUPC ?? "") // Provide a default value if scannedUPC is nil
+//                        
+//                    }
+//                  
+//
+//                case .searchState:
+//                    SearchPage()
+//                case .settingsState:
+//                    SettingsPage()
+//                }
+//            }
+//            NavBarView(x: 32, y: 32, displayState: $currentView)
+//        }
+//    }
+//}
+//
 
 
 #Preview {
